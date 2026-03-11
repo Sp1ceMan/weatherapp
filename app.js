64230cfedf74eeef48b12b8de65060005478b259
Faizulla Nazyrulayev <fayzulla20003@gmail.com>
2026-03-11 22:47:41 +0500

    Соединил html объекты с js

---
+++ app.js
@@ -0,0 +1,4 @@
+const tempElement = document.getElementById('temp');
+const windElement = document.getElementById('wind');
+const precipitationElement = document.getElementById('precipitation');
+const iconElement = document.getElementById('icon');
