42c73f327088073caa87314454c2c9eb9ccbb1eb
Faizulla Nazyrulayev <fayzulla20003@gmail.com>
2026-03-11 22:51:10 +0500

    Добавил ai ollama по fetch, ии запущен локально на личном ПК.

--- app.js
+++ app.js
@@ -32,7 +32,34 @@ async function getWeather() {
     tempElement.textContent = `Температура: ${json.current.temperature_2m}°C`;
     windElement.textContent = `Ветер: ${json.current.wind_speed_10m} км/ч`;
 
+    const promptText = `Ты погодный помощник. Сейчас в городе ${cityName}: температура ${json.current.temperature_2m}°C, ветер ${json.current.wind_speed_10m} км/ч. Дай короткий совет (1-2 предложения): что надеть и нужен ли зонт?`;
+    try {
+      const responseAi = await fetch("http://localhost:11434/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          model: "qwen3:8b",
+          prompt: promptText,
+          stream: false,
+        })
+      });
 
+      if (responseAi.ok) {
+        const dataAi = await responseAi.json();
+        document.getElementById('ai-response').textContent = dataAi.response;
+      } else {
+        const errorText = await responseAi.text();
+        console.error("Ответ сервера:", errorText);
+      }
+    } catch (error) {
+      console.error("Ошибка связи с Ollama:", error);
+    }
+  } else {
+    alert("Ошибка HTTP:" + response.status);
+  }
+}
 
 const searchBtn = document.querySelector('button');
 const cityInput = document.querySelector('input');
